1eaf5c576b670ec82d36f0d2534f4cd8449a7c38
Daniel Oliveira <daniel_oliveira110@hotmail.com>
2024-05-30 20:02:33 +0000

    update

---
+++ Big-data/Home.py
@@ -0,0 +1,181 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+import time
+st.set_option('deprecation.showPyplotGlobalUse', False)
+import plotly.graph_objs as go
+
+#uncomment this line if you use mysql
+#from query import *
+
+st.set_page_config(page_title="Dashboard",page_icon="🌍",layout="wide")
+st.header("ANALYTICAL PROCESSING, KPI, TRENDS & PREDICTIONS")
+
+#all graphs we use custom css not streamlit 
+theme_plotly = None 
+
+
+# load Style css
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+
+#uncomment these two lines if you fetch data from mysql
+#result = view_all_data()
+#df=pd.DataFrame(result,columns=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating","id"])
+
+#load excel file | comment this line when  you fetch data from mysql
+df=pd.read_excel('data.xlsx', sheet_name='Sheet1')
+
+#side bar logo
+
+
+#switcher
+
+region=st.sidebar.multiselect(
+    "SELECT REGION",
+     options=df["Region"].unique(),
+     default=df["Region"].unique(),
+)
+location=st.sidebar.multiselect(
+    "SELECT LOCATION",
+     options=df["Location"].unique(),
+     default=df["Location"].unique(),
+)
+construction=st.sidebar.multiselect(
+    "SELECT CONSTRUCTION",
+     options=df["Construction"].unique(),
+     default=df["Construction"].unique(),
+)
+
+df_selection=df.query(
+    "Region==@region & Location==@location & Construction ==@construction"
+)
+
+
+def graphs():
+    #total_investment=int(df_selection["Investment"]).sum()
+    #averageRating=int(round(df_selection["Rating"]).mean(),2) 
+    #simple bar graph  investment by business type
+    investment_by_business_type=(
+        df_selection.groupby(by=["BusinessType"]).count()[["Investment"]].sort_values(by="Investment")
+    )
+    fig_investment=px.bar(
+       investment_by_business_type,
+       x="Investment",
+       y=investment_by_business_type.index,
+       orientation="h",
+       title="<b> INVESTMENT BY BUSINESS TYPE </b>",
+       color_discrete_sequence=["#0083B8"]*len(investment_by_business_type),
+       template="plotly_white",
+    )
+    fig_investment.update_layout(
+     plot_bgcolor="rgba(0,0,0,0)",
+     font=dict(color="black"),
+     yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color  
+     paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+     xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+     )
+
+    #simple line graph investment by state
+    investment_state=df_selection.groupby(by=["State"]).count()[["Investment"]]
+    fig_state=px.line(
+       investment_state,
+       x=investment_state.index,
+       y="Investment",
+       orientation="v",
+       title="<b> INVESTMENT BY STATE </b>",
+       color_discrete_sequence=["#0083b8"]*len(investment_state),
+       template="plotly_white",
+    )
+    fig_state.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False))
+     )
+
+    left,right,center=st.columns(3)
+    left.plotly_chart(fig_state,use_container_width=True)
+    right.plotly_chart(fig_investment,use_container_width=True)
+    
+    with center:
+      #pie chart
+      fig = px.pie(df_selection, values='Rating', names='State', title='RATINGS BY REGIONS')
+      fig.update_layout(legend_title="Regions", legend_y=0.9)
+      fig.update_traces(textinfo='percent+label', textposition='inside')
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
+#function to show current earnings against expected target     
+def Progressbar():
+    st.markdown("""<style>.stProgress > div > div > div > div { background-image: linear-gradient(to right, #99ff99 , #FFFF00)}</style>""",unsafe_allow_html=True,)
+    target=3000000000
+    current=df_selection["Investment"].sum()
+    percent=round((current/target*100))
+    mybar=st.progress(0)
+
+    if percent>100:
+        st.subheader("Target done !")
+    else:
+     st.write("you have ",percent, "% " ,"of ", (format(target, 'd')), "TZS")
+     for percent_complete in range(percent):
+        time.sleep(0.1)
+        mybar.progress(percent_complete+1,text=" Target Percentage")
+
+#menu bar
+def sideBar():
+ with st.sidebar:
+    selected=option_menu(
+        menu_title="Main Menu",
+        options=["Home","Progress"],
+        icons=["house","eye"],
+        menu_icon="cast",
+        default_index=0
+    )
+ if selected=="Home":
+    #st.subheader(f"Page: {selected}")
+    Home()
+    graphs()
+ if selected=="Progress":
+    #st.subheader(f"Page: {selected}")
+    Progressbar()
+    graphs()
+
+sideBar()
+st.sidebar.image("data/logo1.png",caption="")
+
+
+st.subheader('PICK FEATURES TO EXPLORE DISTRIBUTIONS TRENDS BY QUARTILES',)
+#feature_x = st.selectbox('Select feature for x Qualitative data', df_selection.select_dtypes("object").columns)
+feature_y = st.selectbox('Select feature for y Quantitative Data', df_selection.select_dtypes("number").columns)
+fig2 = go.Figure(
+    data=[go.Box(x=df['BusinessType'], y=df[feature_y])],
+    layout=go.Layout(
+        title=go.layout.Title(text="BUSINESS TYPE BY QUARTILES OF INVESTMENT"),
+        plot_bgcolor='rgba(0, 0, 0, 0)',  # Set plot background color to transparent
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+        xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+        yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color
+        font=dict(color='#cecdcd'),  # Set text color to black
+    )
+)
+# Display the Plotly figure using Streamlit
+st.plotly_chart(fig2,use_container_width=True)
+
+
+
+#theme
+hide_st_style=""" 
+
+<style>
+#MainMenu {visibility:hidden;}
+footer {visibility:hidden;}
+header {visibility:hidden;}
+</style>
+"""
+
+
+
+
+
+
