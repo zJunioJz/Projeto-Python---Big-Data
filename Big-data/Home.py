fc97cb3d555f5d46e4a49c1c21815304b315ad4b
Daniel Oliveira <daniel_oliveira110@hotmail.com>
2024-06-12 01:42:36 +0000

    Update

--- Big-data/Home.py
+++ Big-data/Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-# Configurar layout da página
 st.set_page_config(page_title="Medidas Fisiológicas", page_icon="", layout="wide")
 
 st.sidebar.markdown(
@@ -28,50 +27,41 @@ st.sidebar.image("logo.png", use_column_width=True)
 
 st.success("**Medidas Fisiológicas**")
 
-# Carregar dados do Excel
+
 tabela_fisio = pd.read_excel('Dados.xlsx', sheet_name='Medidas fisiológicas', nrows=350)
 tabela_fisio = tabela_fisio[['Turma', 'Nome', 'FC rep', 'PA rep', 'FCmáx polar', 'FCmáx teste', 'Teste FC', 'FCmáx prev.', 'FC Polar leve', 'FC Polar mod.', 'FC Polar méd.', 'FC Polar forte', 'FC Polar máx', 'FCteste leve', 'FCteste mod.', 'FCteste méd.', 'FCteste forte', 'FCteste máx', 'FCprev leve', 'FCprev mod.', 'FCprev méd.', 'FCprev forte', 'FCprev máx']]
 
-# Substituir valores não identificados por "-"
-tabela_fisio = tabela_fisio.replace(',', '-')
-
-# Substituir "-" por " - " para manter o intervalo
-tabela_fisio = tabela_fisio.replace(',', ' - ', regex=True)
-
-# Converter para tipo numérico, ignorando os valores não numéricos
-tabela_fisio = tabela_fisio.apply(pd.to_numeric, errors='ignore')
 
 for col in tabela_fisio.columns[2:]:
     tabela_fisio[col] = pd.to_numeric(tabela_fisio[col], errors='coerce')
 
-# Carregar estilo CSS
+
 with open('style.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# Selecionar turma
+
 selected_turma = st.selectbox('Selecione a Turma', tabela_fisio['Turma'].unique())
 
-# Filtrar alunos da turma selecionada
+
 alunos_turma = tabela_fisio[tabela_fisio['Turma'] == selected_turma]['Nome'].unique()
 
-# Selecionar aluno
+
 selected_aluno = st.selectbox('Selecione o aluno', alunos_turma)
 
-# Filtrar dados do aluno selecionado
+
 aluno_data_fisio = tabela_fisio[(tabela_fisio['Turma'] == selected_turma) & (tabela_fisio['Nome'] == selected_aluno)]
 
-# Selecionar colunas para exibir
+
 colunas_disponiveis_fisio = ['FC rep', 'PA rep', 'FCmáx polar', 'FCmáx teste', 'Teste FC', 'FCmáx prev.', 'FC Polar leve', 'FC Polar mod.', 'FC Polar méd.', 'FC Polar forte', 'FC Polar máx', 'FCteste leve', 'FCteste mod.', 'FCteste méd.', 'FCteste forte', 'FCteste máx', 'FCprev leve', 'FCprev mod.', 'FCprev méd.', 'FCprev forte', 'FCprev máx']
 colunas_selecionadas_fisio = st.multiselect("Selecione as colunas para exibir", colunas_disponiveis_fisio, default=colunas_disponiveis_fisio)
 
 st.write("### Todos os Dados")
 st.dataframe(tabela_fisio[['Nome'] + colunas_selecionadas_fisio])
 
-# Mostrar os dados do aluno selecionado com as colunas selecionadas
 st.write("### Dados do Aluno Selecionado")
 st.dataframe(aluno_data_fisio[['Nome'] + colunas_selecionadas_fisio])
 
-# Visualização com Plotly para o aluno selecionado, com colunas específicas
+
 colunas_grafico_fisio = ['FC rep', 'PA rep', 'FCmáx polar', 'FCmáx teste', 'Teste FC']
 if colunas_grafico_fisio:
     fig_fisio = go.Figure()
@@ -85,8 +75,8 @@ if colunas_grafico_fisio:
         text=aluno_data_fisio['FC rep'],
         textposition='auto',
         textfont=dict(
-        size=15  # Tamanho da fonte para o texto das barras
-    )  
+            size=15  # Tamanho da fonte para o texto das barras
+        )
     ))
 
     fig_fisio.add_trace(go.Bar(
@@ -98,8 +88,8 @@ if colunas_grafico_fisio:
         text=aluno_data_fisio['PA rep'],
         textposition='auto',
         textfont=dict(
-        size=15  
-    )
+            size=15  
+        )
     ))
 
     fig_fisio.add_trace(go.Bar(
@@ -111,7 +101,7 @@ if colunas_grafico_fisio:
         text=aluno_data_fisio['FCmáx polar'],
         textposition='auto',
         textfont=dict(
-        size=15
+            size=15
         )
     ))
 
@@ -124,8 +114,8 @@ if colunas_grafico_fisio:
         text=aluno_data_fisio['FCmáx teste'],
         textposition='auto',
         textfont=dict(
-        size=15  
-    )
+            size=15  
+        )
     ))
 
     fig_fisio.add_trace(go.Bar(
@@ -137,19 +127,22 @@ if colunas_grafico_fisio:
         text=aluno_data_fisio['Teste FC'],
         textposition='auto',
         textfont=dict(
-        size=15  
-    )
+            size=15  
+        )
     ))
 
-    # Atualizando o layout para espaçar as barras
+
     fig_fisio.update_layout(
-        title='Dados Fisiológicos do Aluno',
+        title=dict(
+            text='Dados Fisiológicos do Aluno',
+            x=0.5  # Centralizar o título
+        ),
         barmode='group',
-        bargap=0.60,  # Espaçamento entre grupos de barras
-        bargroupgap=0.1,  # Espaçamento entre as barras dentro de um grupo
+        bargap=0.60,  
+        bargroupgap=0.1, 
         xaxis=dict(
             tickfont=dict(
-                size=20  # Tamanho da fonte para o eixo x
+                size=20  
             )
         ),
         yaxis=dict(
@@ -161,4 +154,57 @@ if colunas_grafico_fisio:
 
     st.plotly_chart(fig_fisio, use_container_width=True)
 
-#usar para rodar > python -m streamlit run Medidas Fisiológicas.py
+    # Substituir valores infinitos por NaN antes de calcular a média
+    tabela_fisio.replace([float('inf'), float('-inf')], pd.NA, inplace=True)
+
+    # Calcular a média da turma para as colunas selecionadas e arredondar para 2 casas decimais
+    media_turma = tabela_fisio[tabela_fisio['Turma'] == selected_turma][colunas_grafico_fisio].mean().round(2)
+
+    # Gráfico comparando o aluno com a média da turma
+    fig_comparacao = go.Figure()
+
+    fig_comparacao.add_trace(go.Bar(
+        x=colunas_grafico_fisio,
+        y=aluno_data_fisio[colunas_grafico_fisio].values.flatten(),
+        name='Aluno',
+        marker_color='darkblue',
+        text=aluno_data_fisio[colunas_grafico_fisio].values.flatten(),
+        textposition='auto',
+        textfont=dict(
+            size=15
+        )
+    ))
+
+    fig_comparacao.add_trace(go.Bar(
+        x=colunas_grafico_fisio,
+        y=media_turma.values,
+        name='Média da Turma',
+        marker_color='lightblue',
+        text=media_turma.values,
+        textposition='auto',
+        textfont=dict(
+            size=15
+        )
+    ))
+
+    fig_comparacao.update_layout(
+        title=dict(
+            text='Comparação do Aluno com a Média da Turma',
+            x=0.5  # Centralizar o título
+        ),
+        barmode='group',
+        bargap=0.15,  # Espaçamento entre grupos de barras
+        bargroupgap=0.1,  # Espaçamento entre as barras dentro de um grupo
+        xaxis=dict(
+            tickfont=dict(
+                size=15  # Tamanho da fonte para o eixo x
+            )
+        ),
+        yaxis=dict(
+            tickfont=dict(
+                size=15  # Tamanho da fonte para o eixo y
+            )
+        )
+    )
+
+    st.plotly_chart(fig_comparacao, use_container_width=True)
--- Big-data/pages/outros dados.py
+++ Big-data/pages/outros dados.py
@@ -2,17 +2,22 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
 df = pd.read_excel('Gráfico atualizado.xlsx', sheet_name='Dados Cadastrais', nrows=351)
 df = df[['Nome', 'Sexo', 'Turma', 'Idade -Cálculo média']]
+
 
 st.set_page_config(page_title="Home", page_icon="", layout="wide")
 st.success("Outros gráficos ")
 
+
 with open('style.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 idade_counts = df['Idade -Cálculo média'].value_counts().reset_index()
 idade_counts.columns = ['Idade', 'Count']
+
 
 fig = px.pie(idade_counts, values='Count', names='Idade', title='Média Total das Idades')
 fig.update_traces(
@@ -20,33 +25,64 @@ fig.update_traces(
     textinfo='percent+label'
 )
 fig.update_layout(
-        font=dict(
-            size=15  # Tamanho da fonte
-        )
-    )
+    font=dict(
+        size=15  
+    ),
+    title={
+        'text': 'Média Total das Idades',
+        'x': 0.5  # Centralizar o título
+    }
+)
 st.plotly_chart(fig, use_container_width=True)
 
-color_discrete_map = {'M': 'Blue', 'F': 'pink'}
-fig_sexo = px.histogram(df, x='Turma',nbins=30,color='Sexo',title='Distribuição do sexo por turma',
+# Gráfico de histograma de distribuição do sexo por turma
+color_discrete_map = {'M': 'Blue', 'F': 'Pink'}
+fig_sexo = px.histogram(df, x='Turma', nbins=30, color='Sexo', title='Distribuição do sexo por turma',
                         color_discrete_map=color_discrete_map, text_auto=True)
 fig_sexo.update_layout(
     plot_bgcolor='white',
-    xaxis_title='Sexo',
+    xaxis_title='Turma',
     yaxis_title='Frequência',
     bargap=0.1,
+    title={
+        'text': 'Distribuição do sexo por turma',
+        'x': 0.5  # Centralizar o título
+    },
     xaxis=dict(
-            tickfont=dict(
-                size=20  # Tamanho da fonte para o eixo x
-            )
-        ),
-        yaxis=dict(
-            tickfont=dict(
-                size=20  # Tamanho da fonte para o eixo y
-            )
-      ),
-      font=dict(
-            size=15  # Tamanho da fonte
+        tickfont=dict(
+            size=20  
         )
+    ),
+    yaxis=dict(
+        tickfont=dict(
+            size=20  
+        )
+    ),
+    font=dict(
+        size=15  
+    )
 )
 st.plotly_chart(fig_sexo, use_container_width=True)
 
+st.sidebar.markdown(
+    """
+    <style>
+        .sidebar .sidebar-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .sidebar .sidebar-content .block-container {
+            margin-top: 20px;
+        }
+        .sidebar .sidebar-content .stImage {
+            margin-bottom: 20px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.sidebar.image("logo.png", use_column_width=True)
+
+# Usar para rodar > python -m streamlit run Home.py
