697c5e77efa9fedab2c4f90328ff41b88173128a
zJunioJz <junioj253@gmail.com>
2024-07-07 15:40:12 -0300

    teste

--- Big-data/pages/Gráfico de tempo.py
+++ Big-data/pages/Gráfico de tempo.py
@@ -37,103 +37,106 @@ uploaded_file = st.sidebar.file_uploader("Carregar arquivo Excel", type=["xlsx"]
 if uploaded_file is not None:
     # Leitura do arquivo Excel
     tabela = pd.read_excel(uploaded_file, sheet_name='Aptidão Física (2)', nrows=350)
-    tabela = tabela[['Nome', 'Turma', 'Shuttle run', 'Velocidade / aceleração', 'Tempo de reação direita', 'Tempo de reação 1 direita', 'Tempo de reação 2 direita', 'Tempo de reação 3 direita', 'Tempo de reação esquerda', 'Tempo de reação esquerda 1', 'Tempo de reação esquerda 2', 'Tempo de reação esquerda 3']]
+    
+    # Definindo as colunas necessárias
+    colunas_necessarias = [
+        'Nome', 'Turma', 'Shuttle run', 'Velocidade / aceleração', 
+        'Tempo de reação direita', 'Tempo de reação 1 direita', 
+        'Tempo de reação 2 direita', 'Tempo de reação 3 direita', 
+        'Tempo de reação esquerda', 'Tempo de reação esquerda 1', 
+        'Tempo de reação esquerda 2', 'Tempo de reação esquerda 3'
+    ]
+    
+    # Verificar se todas as colunas necessárias estão presentes
+    colunas_faltantes = [coluna for coluna in colunas_necessarias if coluna not in tabela.columns]
 
-    # Aplica o estilo do arquivo CSS
-    try:
-        with open('/mount/src/projeto-python---big-data/Big-data/style.css') as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    except FileNotFoundError:
-        st.error("Arquivo de estilo não encontrado.")
+    if colunas_faltantes:
+        st.error(f"Colunas faltantes no arquivo: {', '.join(colunas_faltantes)}")
+    else:
+        tabela = tabela[colunas_necessarias]
 
-    # Seleciona a turma
-    selected_turma = st.selectbox('Selecione a Turma', tabela['Turma'].unique())
+        # Aplica o estilo do arquivo CSS
+        try:
+            with open('/mount/src/projeto-python---big-data/Big-data/style.css') as f:
+                st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        except FileNotFoundError:
+            st.error("Arquivo de estilo não encontrado.")
 
-    # Filtra alunos da turma selecionada
-    turma_data = tabela[tabela['Turma'] == selected_turma]
+        # Seleciona a turma
+        selected_turma = st.selectbox('Selecione a Turma', tabela['Turma'].unique())
 
-    # Seleciona o aluno
-    selected_aluno = st.selectbox('Selecione o aluno', turma_data['Nome'].unique())
+        # Filtra alunos da turma selecionada
+        turma_data = tabela[tabela['Turma'] == selected_turma]
 
-    # Filtra dados do aluno selecionado
-    aluno_data = turma_data[turma_data['Nome'] == selected_aluno]
+        # Seleciona o aluno
+        selected_aluno = st.selectbox('Selecione o aluno', turma_data['Nome'].unique())
 
-    # Seleciona as colunas para exibir
-    colunas_disponiveis = ['Shuttle run', 'Velocidade / aceleração', 'Tempo de reação direita', 'Tempo de reação 1 direita', 'Tempo de reação 2 direita', 'Tempo de reação 3 direita', 'Tempo de reação esquerda', 'Tempo de reação esquerda 1', 'Tempo de reação esquerda 2', 'Tempo de reação esquerda 3', 'tempo de sustentação']
-    colunas_selecionadas = st.multiselect("Selecione as colunas para exibir", colunas_disponiveis, default=colunas_disponiveis)
+        # Filtra dados do aluno selecionado
+        aluno_data = turma_data[turma_data['Nome'] == selected_aluno]
 
-    st.write("### Todos os Dados")
-    st.dataframe(tabela[['Nome'] + colunas_selecionadas])
+        # Seleciona as colunas para exibir
+        colunas_disponiveis = colunas_necessarias[2:]  # Exclui 'Nome' e 'Turma'
+        colunas_selecionadas = st.multiselect("Selecione as colunas para exibir", colunas_disponiveis, default=colunas_disponiveis)
 
-    st.write("### Dados do Aluno Selecionado")
-    st.dataframe(aluno_data[['Nome'] + colunas_selecionadas])
+        st.write("### Todos os Dados")
+        st.dataframe(tabela[['Nome'] + colunas_selecionadas])
 
-    # Calcula a média da turma
-    turma_mean = turma_data[colunas_selecionadas].mean().reset_index()
-    turma_mean.columns = ['Métrica', 'Média da Turma']
+        st.write("### Dados do Aluno Selecionado")
+        st.dataframe(aluno_data[['Nome'] + colunas_selecionadas])
 
-    # Prepara os dados do aluno para a comparação
-    aluno_data_selecionadas = aluno_data[colunas_selecionadas].melt(var_name='Métrica', value_name='Valor do Aluno')
-    aluno_data_selecionadas['Nome'] = selected_aluno
-    comparacao_df = pd.merge(aluno_data_selecionadas, turma_mean, on='Métrica')
+        # Calcula a média da turma
+        turma_mean = turma_data[colunas_selecionadas].mean().reset_index()
+        turma_mean.columns = ['Métrica', 'Média da Turma']
 
-    # Plotar gráfico "Dados de tempo do aluno"
-    if colunas_selecionadas:
-        fig = px.bar(aluno_data, x='Nome', y=colunas_selecionadas, barmode='group', title='Dados de tempo do aluno', text_auto=True)
-        fig.update_layout(
-            title={
-                'text': 'Dados de tempo do aluno',
-                'x': 0.45  # Posição centralizada
-            },
-            bargap=0.60,     
-            bargroupgap=0.1,
-            xaxis=dict(
-                tickfont=dict(
-                    size=20  
-                )
-            ),
-            yaxis=dict(
-                tickfont=dict(
-                    size=20  
-                )
-            ),
-            font=dict(
-                size=15  
+        # Prepara os dados do aluno para a comparação
+        aluno_data_selecionadas = aluno_data[colunas_selecionadas].melt(var_name='Métrica', value_name='Valor do Aluno')
+        aluno_data_selecionadas['Nome'] = selected_aluno
+        comparacao_df = pd.merge(aluno_data_selecionadas, turma_mean, on='Métrica')
+
+        # Plotar gráfico "Dados de tempo do aluno"
+        if colunas_selecionadas:
+            fig = px.bar(aluno_data, x='Nome', y=colunas_selecionadas, barmode='group', title='Dados de Tempo do Aluno', text_auto=True)
+            fig.update_layout(
+                title={
+                    'text': 'Dados de Tempo do Aluno',
+                    'x': 0.5  # Centraliza o título
+                },
+                bargap=0.3,  # Ajusta o espaço entre as barras
+                bargroupgap=0.1,  # Ajusta o espaço entre grupos de barras
+                xaxis=dict(
+                    tickfont=dict(size=14),
+                    title='Nome'
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14),
+                    title='Tempo'
+                ),
+                font=dict(size=12)
             )
-        )
 
-        for trace in fig.data:
-            trace.width = 0.10  
+            st.plotly_chart(fig, use_container_width=True)
 
-        st.plotly_chart(fig, use_container_width=True)
-
-    # Plotar gráfico "Comparação de Tempo do Aluno"
-    if colunas_selecionadas:
-        fig = px.bar(comparacao_df, x='Métrica', y=['Valor do Aluno', 'Média da Turma'], barmode='group', title=f'Comparação de Tempo do Aluno ({selected_aluno}) com a Média da Turma ({selected_turma})', text_auto=True)
-        fig.update_layout(
-            title={
-                'text': f'Comparação de Tempo do Aluno ({selected_aluno}) com a Média da Turma ({selected_turma})',
-                'x': 0.35  # Posição centralizada
-            },
-            bargap=0.40,
-            bargroupgap=0.1,
-            xaxis=dict(
-                tickfont=dict(
-                    size=20  # Tamanho da fonte para o eixo x
-                )
-            ),
-            yaxis=dict(
-                tickfont=dict(
-                    size=20  
-                )
-            ),
-            font=dict(
-                size=15  # Tamanho da fonte
+        # Plotar gráfico "Comparação de Tempo do Aluno"
+        if colunas_selecionadas:
+            fig = px.bar(comparacao_df, x='Métrica', y=['Valor do Aluno', 'Média da Turma'], barmode='group', title=f'Comparação de Tempo do Aluno ({selected_aluno}) com a Média da Turma ({selected_turma})', text_auto=True)
+            fig.update_layout(
+                title={
+                    'text': f'Comparação de Tempo do Aluno ({selected_aluno}) com a Média da Turma ({selected_turma})',
+                    'x': 0.5  # Centraliza o título
+                },
+                bargap=0.3,  # Ajusta o espaço entre as barras
+                bargroupgap=0.1,  # Ajusta o espaço entre grupos de barras
+                xaxis=dict(
+                    tickfont=dict(size=14),
+                    title='Métrica'
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14),
+                    title='Tempo'
+                ),
+                font=dict(size=12)
             )
-        )
-        for trace in fig.data:
-            trace.width = 0.30
-        st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True)
 
 else:
     st.warning("Por favor, carregue um arquivo Excel.")
