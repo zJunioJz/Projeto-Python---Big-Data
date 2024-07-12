50f5f7b8d8d0ee644a0a8d816ea61c941659c0d8
zJunioJz <junioj253@gmail.com>
2024-07-12 12:50:43 -0300

    remocao

--- Big-data/pages/Desempenho Academico.py
+++ Big-data/pages/Desempenho Academico.py
@@ -96,8 +96,6 @@ if uploaded_file is not None:
         # Filtra dados do aluno selecionado
         aluno_data = turma_data[turma_data['Nome'] == selected_aluno]
         
-        idade_aluno = aluno_data['Idade -Cálculo média'].values[0]
-        
         # Seleciona as colunas para exibir
         colunas_disponiveis = [coluna for coluna in colunas_necessarias if coluna in tabela.columns][2:]  # Exclui 'Nome' e 'Turma'
         colunas_selecionadas = st.multiselect("Selecione as colunas para exibir", colunas_disponiveis, default=colunas_disponiveis)
@@ -110,7 +108,7 @@ if uploaded_file is not None:
 
         # Exibe os dados cadastrais do aluno selecionado
         st.write(f"### Dados Cadastrais do Aluno: {selected_aluno} - Turma: {aluno_data['Turma'].values[0]}")
-        st.dataframe(aluno_data[['Nome', 'Turma', 'Idade -Cálculo média'] + colunas_selecionadas])
+        st.dataframe(aluno_data[['Nome', 'Turma'] + colunas_selecionadas])
         
         # Calcula a média da turma para cada bimestre
         turma_mean = turma_data[colunas_selecionadas].mean().reset_index()
